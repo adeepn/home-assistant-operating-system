456d2f17e3152a4c35f072b66cc3f0f4606d0929
Viacheslav Bocharov <adeep@lexina.in>
2021-01-03 16:57:04 +0300

    Working 5.7

--- buildroot-external/package/rtl88x2cs/rtl88x2cs.mk
+++ buildroot-external/package/rtl88x2cs/rtl88x2cs.mk
@@ -5,7 +5,9 @@
 ################################################################################
 
 RTL88x2CS_VERSION = main
-RTL88x2CS_SITE = $(call github,chewitt,RTL8822CS,$(RTL88x2CS_VERSION))
+RTL88x2CS_SITE = https://github.com/adeepn/jethome-Build-Armbian/raw/jethome/jethome/overlay/drivers/rtl88x2CS/
+RTL88x2CS_SOURCE = rtl88x2CS_WiFi_linux_v5.9.0.2_36407.20200302_COEX20200103-1717.tar.gz
+#$(call github,chewitt,RTL8822CS,$(RTL88x2CS_VERSION))
 RTL88x2CS_LICENSE = GPL-2.0
 RTL88x2CS_LICENSE_FILES = COPYING
 #RTL88x2CS_MODULE_SUBDIRS = src
